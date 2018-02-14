d53c38912f4f4a93e9e61a88745b4712b459e8cd
ad-b <adam.bykovvski@gmail.com>
2018-02-14 13:21:26 +0100

    Basic log and register features

--- init_db.py
+++ init_db.py
@@ -9,15 +9,16 @@ def db_start():
     create_engine('sqlite:///tmp/test.db', convert_unicode=True)
     db.create_all()
     db.session.commit()
+
     user = models.User()
     user.username = "piotr"
     user.password = bcrypt.generate_password_hash('pppp1234')
     user.email = 'piotr@dyba.com.pl'
     user.admin = True
     user.poweruser = True
+
     db.session.add(user)
     db.session.commit()
-
 
 
 if __name__ == '__main__':
